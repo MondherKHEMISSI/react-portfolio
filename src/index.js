2f207f5cf7ff66a8975163d6bdd7b6d994f4561b
Mondher <monder850@gmail.com>
2023-09-09 02:57:57 +0200

    This onegit add .!

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
     ,
-  </React.StrictMode>
+  </React.StrictMode>,
 )
 
 // If you want to start measuring performance in your app, pass a function
